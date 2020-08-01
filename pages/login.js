2116a763007aab9ac94f2e2d3a9c7e1bc7ae1015
war-tortle <brian.sabatino25@gmail.com>
2020-07-31 22:22:40 -0300

    Navigations between screens, login and account creation are added

--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useMutation, gql } from '@apollo/client';
+
+const AUTENTICAR_USUARIO = gql `
+	mutation autenticarUsuario($input: AutenticarInput) {
+		autenticarUsuario(input: $input) {
+			token
+		}
+	}
+`;
 
 const Login = () => {
+
+	// Routing
+	const router = useRouter();
+	
+	// State para el mensaje
+	const [mensaje, guardarMensaje] = useState(null);
+
+	// Mutation para crear nuevos usuarios en Apollo
+	const [ autenticarUsuario ] = useMutation(AUTENTICAR_USUARIO);
+
+	const formik = useFormik({
+		initialValues: {
+			email: '',
+			password: ''
+		},
+		validationSchema: Yup.object({
+			email: Yup.string()
+					  .email('El email no es válido')
+					  .required('El email no puede ir vacio'),
+			password: Yup.string()
+						 .required('El password es obligatorio')
+		}),
+		onSubmit: async valores => {
+			//console.log(valores);
+			const { email, password } = valores;
+
+			try {
+				const { data } = await autenticarUsuario({
+					variables: {
+						input: {
+							email,
+							password
+						}
+					}
+				});
+				guardarMensaje('Autenticando...');
+
+				// Guardar el token en localstorage
+				const { token } = data.autenticarUsuario;
+				localStorage.setItem('token', token);
+
+				// Redireccionando hacia clientes
+				setTimeout(() => {
+					guardarMensaje(null);
+					router.push('/');
+				}, 2000);				
+
+			} catch(error) {
+				guardarMensaje(error.message.replace('GraphQL error: ', ''));
+
+				setTimeout(() => {
+					guardarMensaje(null);
+				}, 3000);
+			}
+		}
+	})
+
+	const mostrarMensaje = () => {
+		return(
+			<div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+				<p>{mensaje}</p>
+			</div>
+		)
+	}	
+
 	return (
 		<>
 			<Layout>
 				<h1 className="text-center text-2xl text-white font-light">Login</h1>
 
+				{mensaje && mostrarMensaje() }
+
 				<div className="flex justify-center mt-5">
 					<div className="w-full max-w-sm">
-						<form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+						<form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4" onSubmit={formik.handleSubmit}>
 							<div className="mb-4">
 								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="nombre">
 									Email
@@ -19,8 +98,20 @@ const Login = () => {
 									id="email" 
 									type="email" 
 									placeholder="Email Usuario"
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									value={formik.values.email}
 								/>
 							</div>
+
+							{ formik.touched.email && formik.errors.email ? (
+							  	<div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+							  		<p className="font-bold">Error</p>
+							  		<p>{formik.errors.email}</p>
+							  	</div>
+							  ) : null
+							}
+
 							<div className="mb-4">
 								<label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
 									Password
@@ -30,8 +121,20 @@ const Login = () => {
 									id="password" 
 									type="password" 
 									placeholder="Password Usuario"
+									onChange={formik.handleChange}
+									onBlur={formik.handleBlur}
+									value={formik.values.password}
 								/>
 							</div>
+
+							{ formik.touched.password && formik.errors.password ? (
+							  	<div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+							  		<p className="font-bold">Error</p>
+							  		<p>{formik.errors.password}</p>
+							  	</div>
+							  ) : null
+							}
+
 							<input
 								className="bg-gray-800 w-full mt-5 p-2 text-white uppercase hover:bg-gray-900 cursor-pointer"
 								type="submit"
